7dccca40d98152cdd5e2a73c3d70a0b235763870
Brandon Dave <dave.15@wright.edu>
2020-09-16 20:04:23 +0000

    new functions to startup

--- project1/bddStartUp.sh
+++ project1/bddStartUp.sh
@@ -4,13 +4,22 @@
 #Project 1
 #
 #
-# Create PATH if not exist
-if [ ! -d /usr/bin/ ]; then
-	export PATH="$HOME/bin:$PATH"
+# Create PATH if not exist for script directory
+##  Prioritize previous PATH before /scripts
+if [ ! -d $HOME/scripts ]; then
+	export PATH="$PATH:~/scripts"
 	echo "\$PATH created"
 fi
 
+# Create custom alias if necessary
+##  If grep returns true, alias is made
+##  Else create alias and store in .bashrc
+##  Requires refresh on .bashrc file
+if ! grep -q "aws-ssh" .bashrc ;then 
+    	echo alias aws-ssh="'ssh -i BDD-Student_CEG3120_key.pem ubuntu@107.20.210.5'" >> .bashrc
+fi
+
 # Refresh .bashrc
-source ~/.bashrc
+. ~/.bashrc
 echo ".bashrc reloaded"
 
