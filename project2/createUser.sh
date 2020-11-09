2428207b95bb11d62dfef2173e7a44a885162152
BDDave-Student <70174975+BDDave-Student@users.noreply.github.com>
2020-11-09 12:23:35 -0500

    user creation script made

---
+++ project2/createUser.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+## Brandon Dave
+## CEG3120
+## Project2
+#
+# read text file and pass line as input
+# checks if user name is in use
+# creates user quietly with no prompts
+# outputs username and group and password
+read (){
+        if !  grep -q "$1" /etc/passwd ;then
+                #getent group $2 | awk -F ":" '{ print $3 }'
+                #  Uses regex to automate user-added to group
+                adduser --quiet --disabled-password --shell /bin/bash --home /home/$1 --gid $( getent group $2 | awk -F ":" '{ print $3 }' )  --gecos "User" $1
+                
+                userPass=abcdefault12345  #fails simplicity check
+                echo "$1:$userPass" | chpasswd
+                echo "User $1 was created and put in group $2 with the password $userPass"
+        else
+                echo "User $1 has already been created."
+        fi
+}
+
+# outputs error message to user if the bash script is ran improperly.
+usage (){
+        echo "$0 requires a parameter of a file name."
+}
+
+# the user must be using sudo
+if [ "$EUID" -ne 0 ] ;then
+        echo "The user creation script must be ran as root."
+        exit
+fi
+
+# the user must be using a parameter
+if [ -z $1 ] ;then
+        usage
+        exit
+fi
+
+# main init
+if [ -f $1 ] ;then
+        if ! grep -q "devops" /etc/group ;then
+                addgroup devops
+                echo "devops group was created."
+        fi
+
+        for i in $( cat $1);
+        do
+                read $i devops
+                #devops can be substituted for another group to automate adding to group
+        done
+else
+        echo "$1 does not exist."
